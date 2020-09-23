ca4aea4bc6446d93b2b072d9335461c69a98015b
mgrau42 <61083874+mgrau42@users.noreply.github.com>
2020-09-23 21:05:53 +0200

    Update ft_lstclear.c

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -19,11 +19,11 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 
 	if (!del)					/*se comprueba nuestra funcion de borrado*/
 		return ;
-	while (*lst != '\0')		/*se va recorriendo las posiciones de la lista hasta el final*/
+	while (*lst != '\0')				/*se va recorriendo las posiciones de la lista hasta el final*/
 	{
-		del((*lst)->content);	/*se elimina el contenido de la posicion actual*/
+		del((*lst)->content);			/*se elimina el contenido de la posicion actual*/
 		tmp = *lst;				/*se guarda una referencia a la direccion de memoria actual*/
-		*lst = tmp->next;		/*nos movemos a la siguiente posicion, referenciada en next*/
+		*lst = tmp->next;			/*nos movemos a la siguiente posicion, referenciada en next*/
 		free(tmp);				/*ya una vez movido a la siguiente posicion liberamos el espacio de memoria que ya no vamos a usar mas*/
 	}
 	*lst = 0;					/*se añade un nulo en la posicion de la lista donde terminemos de borrar*/
