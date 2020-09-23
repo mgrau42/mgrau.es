11a96b31f19c1e05d1e06ae6cbfa595ca6a62e66
Manuel Grau Bastidas <61083874+mgrau42@users.noreply.github.com>
2020-09-23 21:43:33 +0200

    Update ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -23,11 +23,11 @@ char*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	count = 0;
-	if (!s)									/*se comprueba la cadena pasada como argumento*/
+	if (!s)					/*se comprueba la cadena pasada como argumento*/
 		return (NULL);
 	if (!(str = (char *)malloc(len + 1)))	/*reservamos memoria teniendo en cuenta el + 1  para el final de la cadena*/
 		return (NULL);
-	while (i < start)						/*avanzamos el iterador hasta nuestro punto de inicio*/
+	while (i < start)			/*avanzamos el iterador hasta nuestro punto de inicio*/
 		i++;
 	while ((count < len) && (s[i] != '\0'))	/*copiamos el trozo de cadena deseada*/
 	{
@@ -35,8 +35,8 @@ char*ft_substr(char const *s, unsigned int start, size_t len)
 		i++;
 		count++;
 	}
-	str[count] = '\0';						/*se termina la cadena*/
+	str[count] = '\0';			/*se termina la cadena*/
 	if ((int)start >= ft_strlen((char *)s))	/*en caso de que el start sobepase el largo de la cadena original*/
 		str[0] = '\0';
-	return (str);							/*se devuelve la nueva cadena*/
+	return (str);				/*se devuelve la nueva cadena*/
 }
