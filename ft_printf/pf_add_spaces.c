fc4bb8118a7b35310157757b2b0c9744a3afa1ee
Manuel Grau Bastidas <61083874+mgrau42@users.noreply.github.com>
2020-11-14 12:06:14 +0100

    Update pf_add_spaces.c

--- ft_printf/pf_add_spaces.c
+++ ft_printf/pf_add_spaces.c
@@ -12,10 +12,10 @@
 
 #include "libftprintf.h"
 
-// funcion de impresion comun con la cual se puede imprimir los espacion extra determinados por la precision y el width
+// función de impresión común con la cual se puede imprimir los espacios extras determinados por la precisión y el ancho
 void	add_spaces(t_tab *t, t_flags *flags, char *c)
 {
-	long int	add; // este sera nuestro numero de espacios a imprimir
+	long int	add; // este será nuestro número de espacios a imprimir
 	int			varlen;
 
 	add = 0;
@@ -26,13 +26,13 @@ void	add_spaces(t_tab *t, t_flags *flags, char *c)
 	else if (flags->field_width > varlen)
 		add = flags->field_width - varlen;
 	if (flags->convertf_is_minus == 1)
-		add_general(t, flags, c); /*funcion de impresion general*/
+		add_general(t, flags, c); /*función de impresión general*/
 	print_space(add, &t->len);
 	if (flags->convertf_is_minus == 0)
 		add_general(t, flags, c);
 }
 
-void	print_space(long int add, int *len) /*funcion de impresion de espacios*/
+void	print_space(long int add, int *len) /*función de impresión de espacios*/
 {
 	while (add-- > 0)
 	{
