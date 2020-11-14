bf37e9f1ee751250a0e6b08bd834de01344f5df3
Manuel Grau Bastidas <61083874+mgrau42@users.noreply.github.com>
2020-11-14 12:13:23 +0100

    Update pf_arg_treatment.c

--- ft_printf/pf_arg_treatment.c
+++ ft_printf/pf_arg_treatment.c
@@ -12,25 +12,25 @@
 
 #include "libftprintf.h"
 
-// En esta funcion inicializamos nuestra estructura t_flags que usaremos mas adelante para manejar cada flag de forma mas facil
-// Si existe algun flag conversor se redirige la funcion hacia general_converter
-// La funcion general specifiers se encargara de redirigir la funcion hacia el tratamiento adecuado segun el tipo de variable
+// En esta función inicializamos nuestra estructura t_flags que usaremos más adelante para manejar cada flag de forma más fácil
+// Si existe algún flag conversor se redirige la función hacia general_converter
+// La función general specifiers se encargará de redirigir la función hacia el tratamiento adecuado según el tipo de variable
 char	*arg_treatment(t_tab *t)
 {
 	t_flags flags;
 
-	t->format++; /*avanzamos en la cadena*/
-	if (*t->format == ' ') /*en caso de encontrarnos con uno o mas espacios tras nuestro '%' se escribe el primer espacio y se avanza hasta el primer carcter distinto de espacio*/
+	t->format++; 						/*avanzamos en la cadena*/
+	if (*t->format == ' ') 					/*en caso de encontrarnos con uno o más espacios tras nuestro '%' se escribe el primer espacio y se avanza hasta el primer carácter distinto de espacio*/
 		write(1, t->format, 1);
 	while (*t->format == ' ')
 		t->format++;
-	ft_bzero(&flags, sizeof(flags)); /*se inicializa nuestra estructura poniendo sus elementos a cero*/
-	flags = iniflags(flags); /*se usa para inicializar la lista de especificadores de tipo de variable*/
+	ft_bzero(&flags, sizeof(flags)); 			/*se inicializa nuestra estructura poniendo sus elementos a cero*/
+	flags = iniflags(flags); 				/*se usa para inicializar la lista de especificadores de tipo de variable*/
 	while ((*t->format == '-') || (*t->format == '.')\
-	|| (*t->format == '*') || (ft_isdigit(*t->format))) /*en caso de encontrar algun flag conversor entramos en general converter*/
+	|| (*t->format == '*') || (ft_isdigit(*t->format))) 	/*en caso de encontrar algún flag conversor entramos en general converter*/
 		t->format = general_converter(t->format, &flags, t->args);
-	general_specifiers(t, &flags); /*proseguimos hacia general_specifiers*/
+	general_specifiers(t, &flags); 				/*proseguimos hacia general_specifiers*/
 	if (t->iserror == 1)
 		return (t->format);
-	return (++t->format); /*despues de haber impreso nuestra variable volvemos a nuestra funcion printf principal*/
+	return (++t->format); 					/*después de haber impreso nuestra variable volvemos a nuestra funcion printf principal*/
 }
